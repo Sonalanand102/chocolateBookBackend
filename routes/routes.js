6fe75813b5a828eda170fe66b2bafd5d2d3c4979
9GAG <sonalanand102@gmail.com>
2023-11-07 16:30:37 +0530

    route path changed

--- routes/routes.js
+++ routes/routes.js
@@ -23,7 +23,7 @@ router.post('/post', async (req, res) => {
 })
 
 //Get all Method
-router.get('/getAll', async (req, res) => {
+router.get('/getAllKhataList', async (req, res) => {
     try{
         const data = await Model.find();
         res.json(data)
